52fbc604e8115af0d31b9aa6405de5ff57b73829
mio <10892119+biuuu@users.noreply.github.com>
2021-02-25 12:28:27 +0800

    update

--- src/getData.js
+++ src/getData.js
@@ -8,83 +8,77 @@ const config = require('./config')
 const { enableProxy, disableProxy } = require('./module/system-proxy')
 const mitmproxy = require('node-mitmproxy')
 
+const dataMap = new Map()
 const order = ['301', '302', '200', '100']
 
-let gachaTypesUrl
-let gachaLogUrl
-let uid = 0
-let lang = ''
-let localData = null
+const saveData = async (data, url) => {
+  const obj = Object.assign({}, data)
+  obj.result = [...obj.result]
+  obj.typeMap = [...obj.typeMap]
+  config.urls.set(data.uid, url)
+  config.save()
+  await saveJSON(`gacha-list-${uid}.json`, obj)
+}
 
-// const saveData = async (data) => {
-//   const obj = Object.assign({}, data)
-//   obj.result = [...obj.result]
-//   obj.typeMap = [...obj.typeMap]
-//   await saveJSON(`gacha-list-${uid}.json`, obj)
-//   await saveJSON('gacha-list.json', obj)
-// }
+const defaultTypeMap = new Map([
+  ['301', '角色活动祈愿'],
+  ['302', '武器活动祈愿'],
+  ['200', '常驻祈愿'],
+  ['100', '新手祈愿']
+])
 
-// const itemTypeFixMap = new Map([
-//   ['角色活动祈愿', '301'],
-//   ['武器活动祈愿', '302'],
-//   ['常驻祈愿', '200'],
-//   ['新手祈愿', '100']
-// ])
+let localDataReaded = false
+const readdir = util.promisify(fs.readdir)
+const readData = async () => {
+  if (localDataReaded) return
+  localDataReaded = true
+  await fs.ensureDir(userDataPath)
+  const files = await readdir(userDataPath)
+  for (let name of files) {
+    if (/^gacha-list-\d+\.json$/.test(name)) {
+      try {
+        const data = await readJSON(name)
+        data.typeMap = new Map(data.typeMap) || defaultTypeMap
+        data.result = new Map(data.result)
+        if (data.uid) {
+          dataMap.set(data.uid, data)
+        }
+      } catch (e) {
+        sendMsg(e, 'ERROR')
+      }
+    }
+  }
+}
 
-// const defaultTypeMap = new Map([
-//   ['301', '角色活动祈愿'],
-//   ['302', '武器活动祈愿'],
-//   ['200', '常驻祈愿'],
-//   ['100', '新手祈愿']
-// ])
+const mergeList = (a, b) => {
+  if (!a || !a.length) return b || []
+  if (!b || !b.length) return a
+  const minA = new Date(a[0][0]).getTime()
+  let pos = b.length
+  for (let i = 0; i < b.length; i++) {
+    const time = new Date(b[i][0]).getTime()
+    if (time >= minA) {
+      pos = i
+      break
+    }
+  }
+  return b.slice(0, pos).concat(a)
+}
 
-// const readData = async () => {
-//   try {
-//     const obj = await readJSON(`gacha-list${uid ? `-${uid}` : ''}.json`)
-//     if (!obj) return false
-//     obj.result.forEach(item => {
-//       if (itemTypeFixMap.has(item[0])) {
-//         item[0] = itemTypeFixMap.get(item[0])
-//       }
-//     })
-//     obj.typeMap = obj.typeMap ? new Map(obj.typeMap) : defaultTypeMap
-//     obj.result = new Map(obj.result)
-//     localData = obj
-//     return obj
-//   } catch (e) {
-//     return false
-//   }
-// }
-
-// const mergeList = (a, b) => {
-//   if (!a || !a.length) return b || []
-//   if (!b || !b.length) return a
-//   const minA = new Date(a[0][0]).getTime()
-//   let pos = b.length
-//   for (let i = 0 i < b.length i++) {
-//     const time = new Date(b[i][0]).getTime()
-//     if (time >= minA) {
-//       pos = i
-//       break
-//     }
-//   }
-//   return b.slice(0, pos).concat(a)
-// }
-
-// const mergeData = (local, origin) => {
-//   if (local && local.result) {
-//     const localResult = local.result
-//     const localUid = local.uid
-//     const originUid = origin.uid
-//     if (localUid !== originUid) return origin.result
-//     const originResult = new Map()
-//     for (let [key, value] of origin.result) {
-//       const newVal = mergeList(value, localResult.get(key))
-//       originResult.set(key, newVal)
-//     }
-//   }
-//   return origin.result
-// }
+const mergeData = (local, origin) => {
+  if (local && local.result) {
+    const localResult = local.result
+    const localUid = local.uid
+    const originUid = origin.uid
+    if (localUid !== originUid) return origin.result
+    const originResult = new Map()
+    for (let [key, value] of origin.result) {
+      const newVal = mergeList(value, localResult.get(key))
+      originResult.set(key, newVal)
+    }
+  }
+  return origin.result
+}
 
 const readLog = async () => {
   try {
@@ -115,44 +109,55 @@ const readLog = async () => {
   }
 }
 
-// const getGachaLog = async (key, page, name, retryCount = 5) => {
-//   try {
-//     const res = await request(`${gachaLogUrl}&gacha_type=${key}&page=${page}&size=${20}`)
-//     return res.data.list
-//   } catch (e) {
-//     if (retryCount) {
-//       sendMsg(`获取${name}第${page}页失败，5秒后进行第${6 - retryCount}次重试……`)
-//       await sleep(5)
-//       retryCount--
-//       return await getGachaLog(key, page, name, retryCount)
-//     } else {
-//       sendMsg(`获取${name}第${page}页失败，已超出重试次数`)
-//       throw e
-//     }
-//   }
-// }
+const getGachaLog = async ({ key, page, name, retryCount, url }) => {
+  try {
+    const res = await request(`${url}&gacha_type=${key}&page=${page}&size=${20}`)
+    return res.data.list
+  } catch (e) {
+    if (retryCount) {
+      sendMsg(`获取${name}第${page}页失败，5秒后进行第${6 - retryCount}次重试……`)
+      await sleep(5)
+      retryCount--
+      return await getGachaLog(key, page, name, retryCount, url)
+    } else {
+      sendMsg(`获取${name}第${page}页失败，已超出重试次数`)
+      throw e
+    }
+  }
+}
 
-// const getGachaLogs = async (name, key) => {
-//   let page = 1
-//   let data = []
-//   let res = []
-//   do {
-//     if (page % 10 === 0) {
-//       sendMsg(`正在获取${name}第${page}页，每10页休息1秒……`)
-//       await sleep(1)
-//     }
-//     sendMsg(`正在获取${name}第${page}页`)
-//     res = await getGachaLog(key, page, name)
-//     if (!uid && res.length) {
-//       uid = res[0].uid
-//       config.urls.set(uid, gachaLogUrl)
-//       config.save()
-//     }
-//     data.push(...res)
-//     page += 1
-//   } while (res.length > 0)
-//   return data
-// }
+const getGachaLogs = async ({ name, key }, queryString) => {
+  let page = 1
+  let list = []
+  let res = []
+  let uid = 0
+  const url = `https://hk4e-api.mihoyo.com/event/gacha_info/api/getGachaLog?${queryString}`
+  do {
+    if (page % 10 === 0) {
+      sendMsg(`正在获取${name}第${page}页，每10页休息1秒……`)
+      await sleep(1)
+    }
+    sendMsg(`正在获取${name}第${page}页`)
+    res = await getGachaLog({ key, page, name, url, retryCount: 5 })
+    if (!uid && res.length) {
+      uid = res[0].uid
+    }
+    list.push(...res)
+    page += 1
+  } while (res.length > 0)
+  return { list, uid }
+}
+
+const tryGetUid = async (queryString) => {
+  const url = `https://hk4e-api.mihoyo.com/event/gacha_info/api/getGachaLog?${queryString}`
+  for (let [key] of defaultTypeMap) {
+    const res = await request(`${url}&gacha_type=${key}&page=1&size=6`)
+    if (res.data.list && res.data.list.length) {
+      return res.data.list[0].uid
+    }
+  }
+  return config.current
+}
 
 const getGachaType = async (queryString) => {
   const gachaTypeUrl = `https://hk4e-api.mihoyo.com/event/gacha_info/api/getConfigList?${queryString}`
@@ -182,50 +187,11 @@ const getGachaType = async (queryString) => {
 const getQuerystring = (url) => {
   const { searchParams } = new URL(url)
   if (!searchParams.get('authkey')) {
+    sendMsg('URL中缺少authkey')
     return false
   }
   return searchParams
 }
-
-// const getUrlFromConfig = () => {
-//   if (config.urls.size) {
-//     if (localData && localData.uid && config.urls.has(uid)) {
-//       const url = config.urls.get(uid)
-//       return getQuerystring(url)
-//     }
-//   }
-// }
-
-// const getUrlFromLog = async () => {
-//   const url = await readLog()
-//   return getQuerystring(url)
-// }
-
-
-// const getData = async () => {
-//   const result = new Map()
-//   const typeMap = new Map()
-
-//   gachaTypesUrl = `https://hk4e-api.mihoyo.com/event/gacha_info/api/getConfigList?${queryString}`
-//   gachaLogUrl = `https://hk4e-api.mihoyo.com/event/gacha_info/api/getGachaLog?${queryString}`
-
-//   for (const type of orderedGachaTypes) {
-//     const logs = (await getGachaLogs(type.name, type.key)).map((item) => {
-//       return [item.time, item.name, item.item_type, parseInt(item.rank_type)]
-//     })
-//     logs.reverse()
-//     typeMap.set(type.key, type.name)
-//     result.set(type.key, logs)
-//   }
-//   const data = { result, time: Date.now(), typeMap, uid, lang }
-//   await readData()
-//   const mergedResult = mergeData(localData, data)
-//   data.result = mergedResult
-//   await saveData(data)
-//   return data
-// }
-
-const dataMap = new Map()
 
 const proxyServer = (port) => {
   return new Promise((rev) => {
@@ -247,14 +213,10 @@ const proxyServer = (port) => {
   })
 }
 
-
-
 const useProxy = async () => {
   const ip = localIp()
   const port = config.proxyPort
-
-  sendMsg('正在使用代理模式(${ip}:${port})获取URL，请打开游戏抽卡记录并点击抽卡记录下一页')
-
+  sendMsg(`正在使用代理模式(${ip}:${port})获取URL，请打开游戏抽卡记录并点击抽卡记录下一页`)
   await enableProxy('127.0.0.1', port)
   const url = await proxyServer(port)
   await disableProxy()
@@ -288,61 +250,67 @@ const getUrl = async () => {
   } else {
     const result = await tryRequest()
     if (!result) {
-      return url
+      url = await readLog()
     }
   }
   if (!url) {
     url = await useProxy()
   } else {
-    const gachaType = await tryGetGachaType()
-    if (!gachaType) {
+    const result = await tryRequest()
+    if (!result) {
       url = await useProxy()
     }
   }
   return url
 }
 
-
 const fetchData = async () => {
   const url = await getUrl()
-
-}
-
-let localDataReaded = false
-const readdir = util.promisify(fs.readdir)
-const readData = async () => {
-  if (localDataReaded) return
-  localDataReaded = true
-  await fs.ensureDir(userDataPath)
-  const files = await readdir(userDataPath)
-  for (let name of files) {
-    if (/^gacha-list-\d+\.json$/.test(name)) {
-      try {
-        const data = await readJSON(name)
-        data.typeMap = new Map(data.typeMap)
-        data.result = new Map(data.result)
-        if (data.uid) {
-          dataMap.set(data.uid, data)
-        }
-      } catch (e) {
-        sendMsg(e, 'ERROR')
-      }
-    }
+  if (!url) return false
+  const searchParams = await getQuerystring(url)
+  if (!searchParams) return false
+  let queryString = searchParams.toString()
+  const vUid = await tryGetUid(queryString)
+  const localLang = dataMap.has(vUid) ? dataMap.get(vUid).lang : ''
+  if (localLang) {
+    searchParams.set('lang', localLang)
   }
+  queryString = searchParams.toString()
+  const gachaType = await getGachaType(queryString)
+
+  const result = new Map()
+  const typeMap = new Map()
+  const lang = searchParams.get('lang')
+  let originUid = 0
+  for (const type of gachaType) {
+    const { list, uid } = await getGachaLogs(type, queryString)
+    const logs = list.map((item) => {
+      return [item.time, item.name, item.item_type, parseInt(item.rank_type)]
+    })
+    logs.reverse()
+    typeMap.set(type.key, type.name)
+    result.set(type.key, logs)
+    originUid = uid
+  }
+  const data = { result, time: Date.now(), typeMap, uid: originUid, lang }
+  await readData()
+  const localData = dataMap.get(originUid)
+  const mergedResult = mergeData(localData, data)
+  data.result = mergedResult
+  await saveData(data, url)
+  return data
 }
 
-// readData()
-
-// ipcMain.handle('FETCH_DATA', async () => {
-//   try {
-//     const data = await getData()
-//     return data
-//   } catch (e) {
-//     sendMsg(e, 'ERROR')
-//     console.error(e)
-//   }
-//   return false
-// })
+ipcMain.handle('FETCH_DATA', async () => {
+  try {
+    const data = await fetchData()
+    return data
+  } catch (e) {
+    sendMsg(e, 'ERROR')
+    console.error(e)
+  }
+  return false
+})
 
 ipcMain.handle('READ_DATA', async () => {
   await readData()
@@ -352,5 +320,5 @@ ipcMain.handle('READ_DATA', async () => {
   }
 })
 
-// exports.readData = readData
+exports.readData = readData
 
