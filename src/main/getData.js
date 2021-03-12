ac0b4c564a8bf73de2ebb539b98a110c1d2b93a4
mio <10892119+biuuu@users.noreply.github.com>
2021-03-12 09:15:20 +0800

    feat: proxy mode captures webstatic instead of hk4e-api

--- src/main/getData.js
+++ src/main/getData.js
@@ -280,13 +280,13 @@ const proxyServer = (port) => {
   return new Promise((rev) => {
     mitmproxy.createProxy({
       sslConnectInterceptor: (req, cltSocket, head) => {
-        if (/hk4e-api([^\.]{2,10})?\.mihoyo\.com/.test(req.url)) {
+        if (/webstatic([^\.]{2,10})?\.mihoyo\.com/.test(req.url)) {
           return true
         }
       },
       requestInterceptor: (rOptions, req, res, ssl, next) => {
         next()
-        if (/hk4e-api([^\.]{2,10})?\.mihoyo\.com/.test(rOptions.hostname)) {
+        if (/webstatic([^\.]{2,10})?\.mihoyo\.com/.test(rOptions.hostname)) {
           if (/authkey=[^&]+/.test(rOptions.path)) {
             rev(`${rOptions.protocol}//${rOptions.hostname}${rOptions.path}`)
           }
