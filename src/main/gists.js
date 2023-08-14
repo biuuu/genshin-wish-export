8f2ac585f4fbb8b57cbd59bb430e3939f122b157
organics <374564811@qq.com>
2023-08-14 09:44:49 +0800

    feat: Support uploading UIGF JSON data to github gist (#214)
    * 添加同步至gists的功能
    
    * 添加同步至gists的功能
    
    * 优化交互逻辑
    
    * 优化交互逻辑
    
    * 优化交互逻辑

---
+++ src/main/gists.js
@@ -0,0 +1,141 @@
+const {ipcMain} = require('electron')
+const getData = require('./getData').getData
+const {version} = require('../../package.json')
+const config = require('./config')
+const {Octokit} = require('@octokit/core')
+const {sendMsg} = require("./utils");
+const fetch = require('electron-fetch').default
+const getTimeString = () => {
+    return new Date().toLocaleString('sv').replace(/[- :]/g, '').slice(0, -2)
+}
+
+const formatDate = (date) => {
+    let y = date.getFullYear()
+    let m = `${date.getMonth() + 1}`.padStart(2, '0')
+    let d = `${date.getDate()}`.padStart(2, '0')
+    return `${y}-${m}-${d} ${date.toLocaleString('zh-cn', {hour12: false}).slice(-8)}`
+}
+
+const fakeIdFn = () => {
+    let id = 1000000000000000000n
+    return () => {
+        id = id + 1n
+        return id.toString()
+    }
+}
+
+const shouldBeString = (value) => {
+    if (typeof value !== 'string') {
+        return ''
+    }
+    return value
+}
+
+const start = async () => {
+    const {dataMap, current} = await getData()
+    const data = dataMap.get(current)
+    if (!data?.result.size) {
+        throw new Error('数据为空')
+    }
+    const fakeId = fakeIdFn()
+    const result = {
+        info: {
+            uid: data.uid,
+            lang: data.lang,
+            export_time: formatDate(new Date()),
+            export_timestamp: Date.now(),
+            export_app: 'genshin-wish-export',
+            export_app_version: `v${version}`,
+            uigf_version: 'v2.2'
+        },
+        list: []
+    }
+    const listTemp = []
+    for (let [type, arr] of data.result) {
+        arr.forEach(item => {
+            listTemp.push({
+                gacha_type: shouldBeString(item[4]) || type,
+                time: item[0],
+                timestamp: new Date(item[0]).getTime(),
+                name: item[1],
+                item_type: item[2],
+                rank_type: `${item[3]}`,
+                id: shouldBeString(item[5]) || '',
+                uigf_gacha_type: type
+            })
+        })
+    }
+    listTemp.sort((a, b) => a.timestamp - b.timestamp)
+    listTemp.forEach(item => {
+        delete item.timestamp
+        result.list.push({
+            ...item,
+            id: item.id || fakeId()
+        })
+    })
+
+    if (!config.gistsToken) {
+        throw new Error('未设置gistsToken')
+    }
+
+    const octokit = new Octokit({
+        request: {fetch: fetch},
+        auth: config.gistsToken
+    })
+
+    // 更新Gist
+    if (config.gistsId) {
+        try {
+            // 检查Gists上的文件是否被删除，如果被删除则进入新增流程
+            const response = await octokit.request('GET /gists/{gist_id}', {
+                gist_id: config.gistsId,
+                headers: {
+                    'X-GitHub-Api-Version': '2022-11-28'
+                }
+            })
+
+            if (config.gistsId === response.data.id) {
+                await octokit.request('PATCH /gists/{gist_id}', {
+                    gist_id: response.data.id,
+                    files: {
+                        // 这里文件名不需要加时间戳，加入时间戳会产生多个文件
+                        [`UIGF_${data.uid}.json`]: {
+                            content: JSON.stringify(result, null, 2)
+                        }
+                    },
+                    headers: {
+                        'X-GitHub-Api-Version': '2022-11-28'
+                    }
+                })
+                return true
+            }
+        } catch (e) {
+            sendMsg(e, 'ERROR')
+            console.error(e)
+        }
+    }
+
+    // 新增Gist
+    const response = await octokit.request('POST /gists', {
+        description: 'genshin-wish-export',
+        'public': false,
+        files: {
+            [`UIGF_${data.uid}.json`]: {
+                content: JSON.stringify(result, null, 2)
+            }
+        },
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
+    })
+    if (response.status === 201) {
+        config["gistsId"] = response.data.id
+        await config.save()
+    }
+
+    return true
+}
+
+ipcMain.handle('EXPORT_UIGF_JSON_GISTS', async () => {
+    return await start()
+})
--- src/renderer/components/Setting.vue
+++ src/renderer/components/Setting.vue
@@ -54,6 +54,19 @@
           <a class="cursor-pointer text-blue-400" @click="openLink('https://github.com/DGP-Studio/Snap.Genshin/wiki/StandardFormat#export_app')">统一可交换祈愿记录标准</a>
         </p>
       </el-form-item>
+      <el-form-item label="导出到Github Gists">
+        <el-input placeholder="请输入内容" v-model="settingForm.gistsToken" :disabled="gistsConfigDisabled">
+          <template #append>
+            <el-button v-show="gistsConfigDisabled" @click="configGistsToken">设置Token</el-button>
+            <el-button v-show="!gistsConfigDisabled" @click="saveGistsToken" type="success" plain class="focus:outline-none">保存Token</el-button>
+          </template>
+        </el-input>
+        <p class="text-gray-400 text-xs m-1.5 leading-normal">该功能用于将抽卡记录同步至Github Gists，单击“设置Token”按钮，本地浏览器将会跳转至GithubTokens设置页面，新增您的个人Token，并打开Gists功能的读写权限，最后将新生成的Token存入这里，单击“保存Token”完成设置</p>
+        <el-button @click="uploadGists" type="success" plain class="focus:outline-none" :disabled="!settingForm.gistsToken" :loading="uploadGistsLoading">同步至Gists</el-button>
+        <p class="text-gray-400 text-xs m-1.5 leading-normal">使用这个功能的前提是您的网络可以正常访问Github Gists。<br>支持的工具参考这个链接：
+          <a class="cursor-pointer text-blue-400" @click="openLink('https://github.com/DGP-Studio/Snap.Genshin/wiki/StandardFormat#export_app')">统一可交换祈愿记录标准</a>
+        </p>
+      </el-form-item>
     </el-form>
     <h3 class="text-lg my-4">{{about.title}}</h3>
     <p class="text-gray-600 text-xs mt-1">{{about.license}}</p>
@@ -63,7 +76,7 @@
 
 <script setup>
 const { ipcRenderer, shell } = require('electron')
-import { reactive, onMounted, computed } from 'vue'
+import { ref, reactive, onMounted, computed } from 'vue'
 
 const emit = defineEmits(['close', 'changeLang'])
 
@@ -81,14 +94,15 @@ const settingForm = reactive({
   proxyMode: true,
   autoUpdate: true,
   fetchFullHistory: false,
-  hideNovice: true
+  hideNovice: true,
+  gistsToken: ''
 })
 
 const text = computed(() => props.i18n.ui.setting)
 const about = computed(() => props.i18n.ui.about)
 
 const saveSetting = async () => {
-  const keys = ['lang', 'logType', 'proxyMode', 'autoUpdate', 'fetchFullHistory', 'hideNovice']
+  const keys = ['lang', 'logType', 'proxyMode', 'autoUpdate', 'fetchFullHistory', 'hideNovice', 'gistsToken']
   for (let key of keys) {
     await ipcRenderer.invoke('SAVE_CONFIG', [key, settingForm[key]])
   }
@@ -110,6 +124,24 @@ const openLink = (link) => shell.openExternal(link)
 
 const exportUIGFJSON = () => {
   ipcRenderer.invoke('EXPORT_UIGF_JSON')
+}
+
+const gistsConfigDisabled = ref(true)
+
+const configGistsToken = () => {
+  gistsConfigDisabled.value = false
+  openLink('https://github.com/settings/personal-access-tokens/new')
+}
+
+const saveGistsToken = async () => {
+  gistsConfigDisabled.value = true
+  await saveSetting()
+}
+
+const uploadGistsLoading = ref(false)
+const uploadGists = async () => {
+  uploadGistsLoading.value = true
+  uploadGistsLoading.value = !(await ipcRenderer.invoke('EXPORT_UIGF_JSON_GISTS'))
 }
 
 onMounted(async () => {
