0226588b1a7b243bce861d235f7839a315fc168d
AnyLeftovers <37920527+yudonglin@users.noreply.github.com>
2024-01-01 17:13:37 -0800

    md5 will be checked to keep item id dict synced

--- src/main/UIGFJson.js
+++ src/main/UIGFJson.js
@@ -50,51 +50,75 @@ const shouldBeString = (value) => {
 }
 
 // a dictionary for looking up item ids
-let itemIdDictionary = null;
+const itemIdDict = new Map()
+// the md5 value for the dictionary
+let itemIdDictMd5 = null
 // the file name for saving item id dictionary
-const itemIdDictionaryFileName = 'item-id-dict.json'
+const itemIdDictFileName = 'item-id-dict.json'
+
+// acquire dictionary based on give language
+const fetchItemIdDict = async (lang = "all") => {
+  // fetch item id dict from api.uigf.org
+  const response = await fetch(`https://api.uigf.org/dict/genshin/${lang}.json`)
+  // update local dict
+  Object.entries(await response.json()).forEach(
+    ([lang, table]) => itemIdDict.set(
+      // assign item id table based on language, and convert all id from number to string
+      lang, new Map(Object.entries(table).map(([name, id]) => [name, String(id)]))
+    )
+  )
+}
+
+// acquire dictionary based on give language
+const fetchItemIdDictMd5 = async (lang = "all") => {
+  const response = await fetch('https://api.uigf.org/md5/genshin')
+  const responseJson = await response.json()
+  return responseJson[lang]
+}
 
 // initialize item id dictionary
 const initLookupTable = async () => {
-  if (!itemIdDictionary) {
-    itemIdDictionary = new Map()
-    // if a locally cached dictionary exists
-    if (existsFile(itemIdDictionaryFileName)) {
-      const data = await readJSON(itemIdDictionaryFileName)
-      if (data) data.forEach(([lang, table]) => itemIdDictionary.set(lang, new Map(table)))
-    }
-    // if a local cache is not found
-    else {
-      // acquire dictionary
-      const response = await fetch('https://api.uigf.org/dict/genshin/all.json')
-      const responseJson = await response.json()
-      Object.entries(responseJson).forEach(
-        ([lang, table]) => itemIdDictionary.set(
-          // assign item id table based on language, and convert all id from number to string
-          lang, new Map(Object.entries(table).map(([name, id]) => [name, String(id)]))
-        )
-      )
-    }
+  // if itemIdDictMd5 != null, that means itemIdDict has been init, no need to do it again
+  if (itemIdDictMd5) {
+    return
   }
+  // obtain dict md5
+  itemIdDictMd5 = await fetchItemIdDictMd5()
+  // if a locally cached dictionary does not exists
+  if (!existsFile(itemIdDictFileName)) {
+    await fetchItemIdDict();
+    return;
+  }
+  // if a locally cached dictionary is found
+  const data = await readJSON(itemIdDictFileName)
+  // if the data is null or the md5 does not match
+  if (!data || data.md5 !== itemIdDictMd5) {
+    // console.log("md5 check failed! Re-fetching...")
+    await fetchItemIdDict()
+    return;
+  }
+  // if the data is valid and the md5 matches
+  // console.log("md5 check success!")
+  data.lang.forEach(([lang, table]) => itemIdDict.set(lang, new Map(table)))
 }
 
 // save item id dictionary
 const saveLookupTable = async () => {
-  await saveJSON(itemIdDictionaryFileName, itemIdDictionary)
+  await saveJSON(itemIdDictFileName, { lang: itemIdDict, md5: itemIdDictMd5 })
 }
 
 // get item id
 const getItemId = async (lang, name) => {
   // fetch item id from api.uigf.org if cannot find it from existing item id dictionary
-  if (!itemIdDictionary.get(lang).has(name)) {
+  if (!itemIdDict.get(lang).has(name)) {
     const response = await fetch(`https://api.uigf.org/identify/genshin/${name}`)
     const responseJson = await response.json()
     if (!responseJson.item_id) {
       throw new Error(`Couldn't find the item_id for the ${name}.`)
     }
-    itemIdDictionary.get(lang).set(name, responseJson.item_id.toString())
+    itemIdDict.get(lang).set(name, responseJson.item_id.toString())
   }
-  return itemIdDictionary.get(lang).get(name)
+  return itemIdDict.get(lang).get(name)
 }
 
 const uigfJson = async () => {
